5ed149a1e24a13828b72044f1a63af7832df1a8c
Yevhen <evgeniy.playid@icloud.com>
2024-04-27 21:33:51 +0300

    Apply flake8 corrections

--- app/parse.py
+++ app/parse.py
@@ -10,9 +10,12 @@ from tqdm import tqdm
 BASE_URL = "https://webscraper.io/"
 URLS = {
     "home": "https://webscraper.io/test-sites/e-commerce/more",
-    "computers": "https://webscraper.io/test-sites/e-commerce/more/computers",
-    "laptops": "https://webscraper.io/test-sites/e-commerce/more/computers/laptops",
-    "tablets": "https://webscraper.io/test-sites/e-commerce/more/computers/tablets",
+    "computers": "https://webscraper.io/test-sites/"
+                 "e-commerce/more/computers",
+    "laptops": "https://webscraper.io/test-sites/"
+               "e-commerce/more/computers/laptops",
+    "tablets": "https://webscraper.io/test-sites/"
+               "e-commerce/more/computers/tablets",
     "phones": "https://webscraper.io/test-sites/e-commerce/more/phones",
     "touch": "https://webscraper.io/test-sites/e-commerce/more/phones/touch",
 }
@@ -36,13 +39,15 @@ class WebScraper:
         self.options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=self.options)
 
-    def click_accept_cookies(self, url):
+    def click_accept_cookies(self, url: str) -> None:
         self.driver.get(url)
-        cookies_accept_button = self.driver.find_element(By.CLASS_NAME, "acceptCookies")
+        cookies_accept_button = self.driver.find_element(
+            By.CLASS_NAME, "acceptCookies"
+        )
         if cookies_accept_button.is_displayed():
             cookies_accept_button.click()
 
-    def scroll_down_page_with_more_button(self, url):
+    def scroll_down_page_with_more_button(self, url: str) -> None:
         self.driver.get(url)
 
         while True:
@@ -56,7 +61,7 @@ class WebScraper:
             finally:
                 break
 
-    def scrape_single_page(self, url) -> [Product]:
+    def scrape_single_page(self, url: str) -> [Product]:
         result = []
         self.driver.get(url)
         products = self.driver.find_elements(
@@ -65,7 +70,9 @@ class WebScraper:
         for product in products:
             result.append(
                 Product(
-                    title=product.find_element(By.CSS_SELECTOR, "a.title").get_attribute("textContent"),
+                    title=product.find_element(
+                        By.CSS_SELECTOR, "a.title"
+                    ).get_attribute("textContent"),
                     description=product.find_element(
                         By.CSS_SELECTOR, "[class*=description]"
                     ).text,
@@ -75,7 +82,9 @@ class WebScraper:
                         ).text.replace("$", "")
                     ),
                     rating=len(
-                        product.find_elements(By.CSS_SELECTOR, "p > span.ws-icon-star")
+                        product.find_elements(
+                            By.CSS_SELECTOR, "p > span.ws-icon-star"
+                        )
                     ),
                     num_of_reviews=int(
                         product.find_element(
@@ -87,17 +96,24 @@ class WebScraper:
         return result
 
     @staticmethod
-    def write_to_csv(products, filename):
-        with open(filename, "w", newline="", encoding="utf-8") as file:
+    def write_to_csv(products: [Product], filename: str) -> None:
+        with open(
+                filename,
+                "w",
+                newline="",
+                encoding="utf-8"
+        ) as file:
             writer = csv.writer(file)
             writer.writerow(PRODUCT_FIELDS)
-            writer.writerows([asdict(product).values() for product in products])
+            writer.writerows(
+                [asdict(product).values() for product in products]
+            )
 
-    def close_driver(self):
+    def close_driver(self) -> None:
         self.driver.quit()
 
 
-def get_all_products():
+def get_all_products() -> None:
     for name, url in tqdm(
         URLS.items(),
         desc="Scrapping pages",
